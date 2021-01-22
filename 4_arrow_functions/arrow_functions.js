feafbf5633b4967c3922340f18cb04fb93328725
Franceline Casanas <Franceline90@gmail.com>
2021-01-21 22:03:33 -0600

    moved and renamed

---
+++ 4_arrow_functions/arrow_functions.js
@@ -0,0 +1,31 @@
+// arrow functions
+
+// 1
+const greet = function() {
+    return 'hello, world';
+}
+
+// can be rewritten as
+const greet = () => 'hello, world'
+const result = greet()
+console.log(result)
+
+// 2
+// const bill = function(products, tax) {
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax
+//     }
+//     return total
+// }
+
+// can be rewritten as
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax
+    }
+    return total
+}
+
+console.log(bill([1, 2, 3], .07))
