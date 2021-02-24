b0c32587eedf0589be8bf6fd423238cd6f01dcba
Franceline Casanas <Franceline90@gmail.com>
2021-02-24 11:34:49 -0600

    line-through when item is clicked on

--- 6_the_dom/event_basics/index.js
+++ 6_the_dom/event_basics/index.js
@@ -1,1 +1,24 @@
-alert('helloooooo nurse')
+// const button = document.querySelector('button')
+
+// button.addEventListener('click', () => {
+//     console.log('You clicked me!')
+// })
+
+
+// get li's
+
+const items = document.querySelectorAll('li')
+
+items.forEach(item => {
+    item.addEventListener('click', (e) => {
+        // console.log('item was clicked')
+    
+        // e is the event
+        // console.log(e)
+
+        // e.target tell us which one (li) we clicked on
+        // console.log(e.target)
+
+        e.target.style.textDecoration = 'line-through'
+    })
+})
